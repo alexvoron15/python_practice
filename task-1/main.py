4ec5ce2e3c1d1d5d438b127bbf65f956b6637f6f
alexvoron15 <sasha.vorona2015@gmail.com>
2021-07-08 23:48:32 +0300

    Створив папки tasl-0 та task-1, в першу папку перемістив туди файл першого завдання, а в другій папкі створив новий файл та виконав надане завдання

---
+++ task-1/main.py
@@ -0,0 +1,30 @@
+print("Введіть строчку: ")
+str = input()
+str_out = list(str)
+digit = []
+chars = []
+for i in str_out:
+    if i.isdigit():
+        digit.append(int(i))
+    else:
+        chars.append(i)
+str_out = ''.join(chars)
+print("Змінена строчка: " + str_out)
+print("Створений список чисел: ", digit)
+
+print("Змінена строчка (Верхній вегістр): ", str_out.title())
+
+digits = []
+index = 0
+for i in digit:
+    d_max = max(digit)
+    if d_max == i:
+        index += 1
+        continue
+    else:
+        res = i**index
+        digits.append(res)
+    index += 1
+
+print("Максимальний елемент списку: ", d_max)
+print("Отриманий список: ", digits)
